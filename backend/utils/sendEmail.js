689505d53c69af9e27132f3f9dfddcbc1951403b
vardan <vardan21110@iiitd.ac.in>
2025-04-27 14:14:11 +0530

    changed emal message when using otp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -10,7 +10,7 @@ const sendEmail = async (to, subject, html) => {
   });
 
   await transporter.sendMail({
-    from: `"Your App Name" <${process.env.EMAIL_USER}>`,
+    from: `"Bloghub" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
